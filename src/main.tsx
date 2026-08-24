41ee3a06901c58d0355ade5da0e4501717355147
Glaydson de Araujo <glaydsonaraujo25@gmail.com>
2026-08-24 14:37:36 -0300

    load accessibility preference styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './enhancements.css';
+import './accessibility.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
